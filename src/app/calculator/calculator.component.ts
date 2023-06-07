5ae4e94f61e55938904b53e3647d3e18fc9b166c
Douglas Nascimento Camargo <douglas.camargo@libera.log.br>
2023-06-07 16:05:49 -0300

    Finalizando script de cálculo imc e estilos

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CalculateIMC } from '../../assets/ts/script';
 
 @Component({
   selector: 'app-calculator',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent {
-
+  calculateImc() {
+    CalculateIMC();
+  }
 }
---
+++ src/assets/ts/script.ts
@@ -0,0 +1,46 @@
+function CalculateIMC() {
+    const pesoInput = document.getElementsByName('peso')[0] as HTMLInputElement;
+    const alturaInput = document.getElementsByName('altura')[0] as HTMLInputElement;
+
+    const peso = parseFloat(pesoInput.value);
+    const altura = parseFloat(alturaInput.value);
+
+    const imc = peso / (altura * altura);
+    setMessagesStyles(imc);
+}
+  
+function getIMCMessage(imc: number): string {
+    if (imc < 18.5) {
+        return "<p class='calculator__message-text'>Você está abaixo do peso.</p>";
+    } else if (imc < 25) {
+        return "<p class='calculator__message-text'>Você está no peso ideal.</p>";
+    } else if (imc < 30) {
+        return "<p class='calculator__message-text'>Você está com sobrepeso.</p>";
+    } else if (imc < 35) {
+        return "<p class='calculator__message-text'>Você está com obesidade grau 1.</p>";
+    } else if (imc < 40) {
+        return "<p class='calculator__message-text'>Você está com obesidade grau 2.</p>";
+    } else {
+        return "<p class='calculator__message-text'>Você está com obesidade grau 3.</p>";
+    }
+}
+
+function setMessagesStyles(imc: number) {
+    const resultado = document.getElementsByClassName('calculator__result')[0] as HTMLElement;
+    resultado.innerHTML = `<p class='calculator__result-text'>Seu Índice de massa corporal é: </p>`;
+    resultado.insertAdjacentHTML('beforeend', imc.toFixed(2));
+    resultado.insertAdjacentHTML('beforeend', getIMCMessage(imc));
+    resultado.style.padding = '1rem';
+    resultado.style.textAlign = 'center';
+
+    const resultadoTexto = document.getElementsByClassName('calculator__result-text')[0] as HTMLElement;    
+    resultadoTexto.style.marginBottom = '.8rem';
+    resultadoTexto.style.fontSize = '16px';
+
+    const mensagemTexto = document.getElementsByClassName('calculator__message-text')[0] as HTMLElement;
+    mensagemTexto.style.marginTop = '.8rem';
+    mensagemTexto.style.fontSize = '16px';
+    mensagemTexto.style.fontWeight = 'bold';
+}
+  
+export { CalculateIMC };
